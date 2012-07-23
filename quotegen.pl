d3f9c5142e1d31ac646effb96e7992226873fd0b
listen2 <getifaddrs@gmail.com>
2012-07-23 18:45:58 -0400

    Add quotegen.pl

---
+++ quotegen.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use POSIX;
+
+sub alphize {
+	return @_[0] if (@_[0] <= 9);
+	return chr(@_[0]+ord('a')-10) if (@_[0] < 36);
+	return alphize(@_[0]%36).alphize(floor(@_[0]/36));
+}
+
+my @lines;
+
+my $i = 0;
+open(INFILE, "<".$ARGV[0]);
+while (<INFILE>) {
+	chomp;
+	push(@lines, "input[name=\"uh\"][value^=\"".alphize($i)."\"] ~ a:after { content: \"".$_."\" }\n");
+	$i++;
+}
+
+for my $f (reverse @lines) {
+	print $f;
+}
