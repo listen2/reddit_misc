43ec1258758a5c1c768272315043e4f644fb0e29
listen2 <getifaddrs@gmail.com>
2013-07-04 15:15:20 -0400

    Add reddit_usercolor.user.js

---
+++ userscripts/reddit_usercolor.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name reddit username colorifier
+// @namespace reddit_listen2
+// @description exactly what you think
+// @include *.reddit.com/*
+// ==/UserScript==
+
+(function(){
+	window.addEventListener("neverEndingLoad", function() {    	
+		colorize();
+	}, false);
+
+	function get_color(name) {
+		var a = [0, 0, 0];
+		for (i = 0; i < name.length; i++) {
+			x = name.charCodeAt(i)-48;
+			a[x%3] = a[x%3]+x;
+		}
+		for (i = 0; i < a.length; i++) {
+			a[i] = Math.floor(0.8*(a[i]%256));
+		}
+		return "rgb(" + a[0] + "," + a[1] + "," + a[2] + ")";
+	}
+
+	function colorize() {
+		users = document.getElementsByClassName("author");			//for usernames
+		for (f = 0; f < users.length; f++) {
+			u = users[f];
+			//u.style.backgroundColor = get_color(u.innerText);
+			u.style.color = get_color(u.innerText);
+			u.style.borderRadius = "3px";
+			u.style.padding = "0 2px 0 2px";
+			//u.style.color = "#000";
+		}
+	}
+
+	colorize();
+})();
