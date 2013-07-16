3488ddf2213a5202376358c12ad11668ed76e8d7
listen2 <getifaddrs@gmail.com>
2013-07-15 21:09:53 -0400

    reddit_hideownlikes: fix JSLint errors

--- userscripts/reddit_hideownlikes.user.js
+++ userscripts/reddit_hideownlikes.user.js
@@ -6,32 +6,35 @@
 // ==/UserScript==
 
 (function(){
-	window.addEventListener("neverEndingLoad", function() {    	
-		filter();
-	}, false);
+	"use strict";
+
+	var c, d;
 
 	function filter() {
+		var items, i;
 		items = document.getElementsByClassName("author");
-		for (i = 0; i < items.length; i ++) {
+		for (i = 0; i < items.length; i++) {
 			if (items[i].innerText === reddit.logged) {
-				if (c.checked)
-					items[i].parentNode.parentNode.parentNode.style.display = "none";
-				else
-					items[i].parentNode.parentNode.parentNode.style.display = "block";
+				if (c.checked)	{ items[i].parentNode.parentNode.parentNode.style.display = "none"; }
+				else				{ items[i].parentNode.parentNode.parentNode.style.display = "block"; }
 			}
 		}
 	}
 
-	var c = document.createElement("input");
+	c = document.createElement("input");
 	c.type = "checkbox";
 	c.id = "hide_own_likes";
 	c.addEventListener("change", filter);
 	document.getElementsByClassName("tabmenu")[0].appendChild(c);
 
-	var d = document.createElement("label");
+	d = document.createElement("label");
 	d.htmlFor = c.id;
 	d.innerText = "hide my submissions";
 	document.getElementsByClassName("tabmenu")[0].appendChild(d);
 
 	filter();
-})();
+
+	window.addEventListener("neverEndingLoad", function() {
+		filter();
+	}, false);
+}());
