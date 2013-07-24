037cfbe5e3d4da227daa3b7d821476b869bd1b16
listen2 <getifaddrs@gmail.com>
2013-07-23 23:03:51 -0400

    reddit_sr_link_new: add multireddit sidebar
    Also, factor out duplicated code.

--- userscripts/reddit_sr_link_new.user.js
+++ userscripts/reddit_sr_link_new.user.js
@@ -4,44 +4,47 @@
 // @description rewrites the $subreddit link in the upper-left to always lead to $subreddit/new
 // @include *.reddit.com/r/*
 // @include *.reddit.com/user/*
-// @include *.reddit.com/new/
+// @include *.reddit.com/new
+// @include *.reddit.com/
 // ==/UserScript==
 
 (function(){
 	"use strict";
 
+	function newize(elems, parens, fn) {
+		for (i = 0; i < elems.length; i++) {
+			d = document.createElement("span");
+			d.style.setProperty("font-size", "small");
+			d.style.setProperty("display", "inline");
+
+			e = document.createElement("a");
+			e.innerHTML = "hot";
+			e.href = elems[i].href;
+
+			if (parens) {
+				d.appendChild(document.createTextNode(" ("));
+				d.appendChild(e);
+				d.appendChild(document.createTextNode(")"));
+			} else {
+				d.appendChild(e);
+			}
+			//elems[i].parentNode.insertBefore(d, elems[i].nextSibling);
+			fn(elems, d);
+
+			elems[i].href += "/new";
+			elems[i].href = elems[i].href.replace("//new", "/new");
+		}
+	}
+
 	var elems = document.getElementsByClassName("redditname pagename");
 	var i, d, e;
-	if (elems.length > 0) {
-		d = document.createElement("span");
-		d.style.setProperty("font-size", "small");
-
-		e = document.createElement("a");
-		e.innerHTML = "hot";
-		e.href = elems[0].firstChild.href;
-
-		d.appendChild(document.createTextNode(" ("));
-		d.appendChild(e);
-		d.appendChild(document.createTextNode(")"));
-		elems[0].insertBefore(d, elems[0].firstChild.nextSibling);
-
-		elems[0].firstChild.href += "new";
-	}
+	newize(elems, true, function(e, d) { e[0].insertBefore(d, e[0].firstChild.nextSibling) });
 
 	elems = document.getElementsByClassName("subreddit");
-	for (i = 0; i < elems.length; i++) {
-		d = document.createElement("span");
-		d.style.setProperty("font-size", "small");
+	newize(elems, true, function(e, d) { e[i].parentNode.insertBefore(d, e[i].nextSibling) });
 
-		e = document.createElement("a");
-		e.innerHTML = "hot";
-		e.href = elems[i].href;
+	elems = document.querySelectorAll(".listing-chooser a")
+	newize(elems, false, function(e, d) { e[i].parentNode.insertBefore(d, e[i].nextSibling) });
 
-		d.appendChild(document.createTextNode(" ("));
-		d.appendChild(e);
-		d.appendChild(document.createTextNode(")"));
-		elems[i].parentNode.insertBefore(d, elems[i].nextSibling);
-
-		elems[i].href += "new";
-	}
+	//TODO handler
 }());
