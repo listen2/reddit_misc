53b582bb2c8cc374c47c49c4821c058b2e6816a0
listen2 <getifaddrs@gmail.com>
2014-10-29 16:58:35 -0400

    Add reddit_untargeter.user.js

---
+++ userscripts/reddit_untargeter.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name reddit untargeter
+// @namespace reddit_listen2
+// @description Opens reddit links in the same tab
+// @include *.reddit.com/*
+// ==/UserScript==
+
+(function(){
+	function untarget() {
+		var elems = document.getElementsByTagName("a");
+		var i;
+		for (i = 0; i < elems.length; i++) {
+			if (elems[i].className.indexOf("blank") > -1) {
+				elems[i].className = elems[i].className.replace(/may-blank(:?-within)?/g , "")
+			}
+		}
+	}
+
+	untarget();
+
+	window.addEventListener("neverEndingLoad", function() {
+		untarget();
+	}, false);
+})();
+
